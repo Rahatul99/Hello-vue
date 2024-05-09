ae93417d9899aa07be12f14a418d3d5b9247d631
Rahatul99 <rahatul99@gmail.com>
2024-05-09 15:43:20 +0600

    Methods

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -3,52 +3,25 @@ export default {
   name: "App",
   data() {
     return {
-      names: ["Bruce", "Clark", "Diana"],
-      fullName: [
-        { first: "don", last: "jon" },
-        { first: "tom", last: "jerry" },
-        { first: "cristiano", last: "ronaldo" },
-      ],
-      players: ["Alvas", "Tony", "Miller", "Rodrigo"],
-      actors: [
-        {
-          name: "Leonardo DiCaprio",
-          movies: ["Inception", "The Wolf of Wall Street"],
-        },
-        { names: "Tom Hanks", movie: ["Forrest Gump", "Cast Away"] },
-        {
-          name: "Meryl Streep",
-          movies: ["The Devil Wears Prada", "Sophie's Choice"],
-        },
-        { name: "Denzel Washington", movie: ["Training Day", "Glory"] },
-        {
-          name: "Scarlett Johansson",
-          movies: ["Lost in Translation", "The Avengers"],
-        },
-      ],
+      baseMultiplier: 5,
     };
+  },
+  methods: {
+    add(a, b, c) {
+      return a + b + c;
+    },
+    multiply(num) {
+      return num * this.baseMultiplier;
+    },
   },
 };
 </script>
 
 <template>
   <div>
-    <!-- <h2 v-for="(name, index) in names" :key="name">{{ index }} {{ name }}</h2>
-    <h2 v-for="(name, index) in fullName" :key="name.first">
-      {{ index }}{{ name.first }} {{ name.last }}
-    </h2>
-
-    <h2 v-for="(actor, index) in actors" :key="actor.name">
-      {{ index }} {{ actor.name }}
-      <h3 v-for="movie in actor.movies" :key="movie">
-        {{ index }} number {{ movie }}
-      </h3>
-    </h2> -->
-
-    <h2 v-for="player in players" :key="player">
-      <!-- {{ player }} -->
-      <h3 v-if="player === 'Tony'">{{ player }}</h3>
-    </h2>
+    <!-- {{ 2 * 5 + 3 }}
+    add method: {{ add(5, 5, 5) }} add method: {{ add(6, 6, 6) }} -->
+    {{ multiply(2) }}
   </div>
 </template>
 
