7f19d16f699c46b08ed08d276049875a9bc3ceb2
Rahatul99 <rahatul99@gmail.com>
2024-05-09 15:12:46 +0600

    conditional rendering

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,54 +1,79 @@
 <script>
 export default {
   name: "App",
-  data(){
-    return{
-      num: -1,
-      display: true,
-      showElement: false
-    }
-  }
-}
+  data() {
+    return {
+      names: ["Bruce", "Clark", "Diana"],
+      fullName: [
+        { first: "don", last: "jon" },
+        { first: "tom", last: "jerry" },
+        { first: "cristiano", last: "ronaldo" },
+      ],
+      players: ["Alvas", "Tony", "Miller", "Rodrigo"],
+      actors: [
+        {
+          name: "Leonardo DiCaprio",
+          movies: ["Inception", "The Wolf of Wall Street"],
+        },
+        { names: "Tom Hanks", movie: ["Forrest Gump", "Cast Away"] },
+        {
+          name: "Meryl Streep",
+          movies: ["The Devil Wears Prada", "Sophie's Choice"],
+        },
+        { name: "Denzel Washington", movie: ["Training Day", "Glory"] },
+        {
+          name: "Scarlett Johansson",
+          movies: ["Lost in Translation", "The Avengers"],
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <template>
-  <h2 v-if="num === 0">The number is zero</h2>
-  <h2 v-else-if="num < 0">This is the negative number</h2>
-  <h2 v-else>The number is not zero </h2>
+  <div>
+    <!-- <h2 v-for="(name, index) in names" :key="name">{{ index }} {{ name }}</h2>
+    <h2 v-for="(name, index) in fullName" :key="name.first">
+      {{ index }}{{ name.first }} {{ name.last }}
+    </h2>
 
-  <template v-if="display">
-    <!-- <div>Hello world</div>
-    <div>Hi world</div>
-    <div>Greetings world</div> -->
-    <br>
+    <h2 v-for="(actor, index) in actors" :key="actor.name">
+      {{ index }} {{ actor.name }}
+      <h3 v-for="movie in actor.movies" :key="movie">
+        {{ index }} number {{ movie }}
+      </h3>
+    </h2> -->
 
-    <h2 v-show="showElement">Using v-show</h2>
-    <h2 v-if="showElement">Using v-if</h2>
-  </template>
+    <h2 v-for="player in players" :key="player">
+      <!-- {{ player }} -->
+      <h3 v-if="player === 'Tony'">{{ player }}</h3>
+    </h2>
+  </div>
 </template>
 
-<style scoped>  
+<style scoped>
 header {
   line-height: 1.5;
 }
 
-.new{
+.new {
   color: green;
 }
-.sold-out{
-  color: red
+.sold-out {
+  color: red;
 }
 .logo {
   display: block;
   margin: 0 auto 2rem;
 }
-.danger{
+.danger {
   color: red;
 }
-.underline{
-  text-decoration: underline ;
+.underline {
+  text-decoration: underline;
 }
-.promoted{
+.promoted {
   font-style: italic;
 }
 
