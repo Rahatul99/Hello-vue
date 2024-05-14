4b313b827a01a7ab027dfed7410a9f16ec1d2f73
Rahatul99 <rahatul99@gmail.com>
2024-05-14 12:59:48 +0600

    Replacing data with reactive

--- hello-world/src/components/Data.vue
+++ hello-world/src/components/Data.vue
@@ -3,20 +3,36 @@
     <h2>option api: {{ o_firstName }}</h2>
     <h2>Composition api: {{ c_firstName }}</h2>
     <h2>{{ greet }}</h2>
+
+    <h2>Reactive composition: {{ reactiveHero }}</h2>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 export default {
   name: "Data",
   setup() {
     const c_firstName = ref("Clark");
     c_firstName.value = "Diana";
     const greet = `Hello ${c_firstName.value}`;
+
+    const state = reactive({
+      firstName: "Princess",
+      lastName: "Diana",
+      heroName: "Wonder woman",
+    });
+
+    // const isLoggedIn = ref(false);  in the below isLoggedInReactive is the replace of it
+    // const isLoggedInReactive = reactive({
+    //   value: false,
+    // });
+
+    const reactiveHero = `Hello ${state.firstName} ${state.lastName} and ${state.heroName}`;
     return {
       c_firstName,
       greet,
+      reactiveHero,
     };
   },
   data() {
