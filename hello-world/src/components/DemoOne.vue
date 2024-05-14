0b97f36eb050d3b9ba2a2f9825a2bb7fdc5b2b3b
Rahatul99 <rahatul99@gmail.com>
2024-05-14 14:25:18 +0600

    Reactivity and toRefs

---
+++ hello-world/src/components/DemoOne.vue
@@ -0,0 +1,46 @@
+<template>
+  <h2>{{ name }}</h2>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  name: "DemoOne",
+  setup() {
+    const name = ref("Vishwas");
+
+    setTimeout(() => {
+      name.value = "Codevolution";
+    }, 2000);
+    return {
+      name,
+    };
+  },
+
+  // it also works
+  // setup() {
+  //   let state = reactive({ name: "Vishwas" });
+
+  //   setTimeout(() => {
+  //     state.name = "Codevolution";
+  //   }, 2000);
+  //   return {
+  //     state,
+  //   };
+  // },
+
+  //but below it is did'nt works
+  // setup() {
+  //   let name =  "Vishwas";
+
+  //   setTimeout(() => {
+  //     name = "Codevolution";
+  //   }, 2000);
+  //   return {
+  //     name,
+  //   };
+  // },
+};
+</script>
+
+<style scoped></style>
---
+++ hello-world/src/components/DemoTwo.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <h2>{{ state.firstName }} and {{ state.lastName }}</h2>
+  </div>
+</template>
+
+<script>
+import { reactive, toRefs } from "vue";
+
+export default {
+  name: "DemoTwo",
+  setup() {
+    const state = reactive({
+      firstName: "Bruce",
+      lastName: "Wayne",
+    });
+
+    setTimeout(() => {
+      state.firstName = "Clark";
+      state.lastName = "Kent";
+    }, 2000);
+
+    toRefs(state);
+
+    return {
+      state,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
+
+<!-- <template>
+  <div>
+    <h2>{{ state.firstName }} and {{ state.lastName }}</h2>
+    <h4>{{ fname }} {{ Iname }}</h4>
+  </div>
+</template>
+
+<script>
+import { reactive } from "vue";
+export default {
+  name: "DemoTwo",
+  setup() {
+    const state = reactive({
+      firstName: "Bruce",
+      lastName: "Wayne",
+    });
+    setTimeout(() => {
+      (state.firstName = "Clark"), (state.lastName = "Kent");
+    }, 2000);
+    return {
+    //but it doesn't works 
+      // firstName: state.firstName,
+      // lastName: state.lastName,
+
+      //it works 
+      state,
+    };
+  },
+  data() {
+    return {
+      fname: "Bruce",
+      Iname: "Wayne",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style> -->
