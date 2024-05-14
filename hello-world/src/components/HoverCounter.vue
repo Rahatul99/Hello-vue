007c53bc50dff41e431ac1f7d6e6884bf9d828c6
Rahatul99 <rahatul99@gmail.com>
2024-05-14 10:20:06 +0600

    mixins

---
+++ hello-world/src/components/HoverCounter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button @mouseover="incrementCount">Hovered {{ count }} times</button>
+  </div>
+</template>
+
+<script>
+import CounterMixin from "../mixins/counter";
+export default {
+  name: "HoverCounter",
+  mixins: [CounterMixin],
+};
+</script>
+
+<style lang="scss" scoped></style>
